e9a86168ff0f0b6dbbef212880b4a6b612fef39c
andersonsimplicio <anderson25eu@gmail.com>
2024-10-13 00:49:18 -0300

    Receitas

--- reicetas-drf/apps/receitas/views.py
+++ reicetas-drf/apps/receitas/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    context = {
+        'title':'Pagina de Receitas',
+        'name':'Home'
+    }
+    return render(request,'receitas/pages/home.html',context)
