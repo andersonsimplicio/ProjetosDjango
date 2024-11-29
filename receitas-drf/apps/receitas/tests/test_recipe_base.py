9c1a137dc823e3b4b9247d021506ee37df4ead19
andersonsimplicio <anderson25eu@gmail.com>
2024-11-29 12:10:54 -0300

    Atualização do receitas-drf + testes

--- receitas-drf/apps/receitas/tests/test_recipe_base.py
+++ receitas-drf/apps/receitas/tests/test_recipe_base.py
@@ -1,33 +1,50 @@
 from django.test import TestCase
 from apps.receitas.models import Categoria, Receita,User
-
+from django.urls import resolve, reverse
 
 class RecipeTestBase(TestCase):
     
     def setUp(self) -> None:
-        category = Categoria.objects.create(name='Categoria')
-        author = User.objects.create_user(
-            first_name='user',
-            last_name='name',
-            username='usernameteste',
-            password='123456',
-            email='username@email.com',
-        )
-        recipe = Receita.objects.create(
-            category=category,
-            author=author,
-            title='Recipe Title',
-            description='Recipe Description',
-            slug='recipe-slug',
-            preparation_time=10,
-            preparation_time_unit='Minutos',
-            servings=5,
-            servings_unit='Porções',
-            preparation_steps='Recipe Preparation Steps',
-            preparation_steps_is_html=False,
-            is_published=True,
-        )
-
+        '''
         # Imprime o ID da receita criada
-        print(f"ID da receita criada: {recipe.id}")
+        print(f"ID da receita criada: {self.recipe.id}")
+        '''
         return super().setUp()
+    
+    def  make_category(self,name='Categoria')->Categoria:
+        return Categoria.objects.create(name=name)
+    
+    def make_author(self,first_name='user',last_name='name',username='usernameteste',password='123456',email='username@email.com')-> User:
+      
+        return  User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            password=password,
+            email=email,
+        )
+    def make_recipe(self,category_data=None,author_data=None,title='Bolo de Chocolate',description='Recipe Description',slug='recipe-slug',
+                    preparation_time=10,preparation_time_unit='Minutos',servings=5,servings_unit='Porções',preparation_steps='Recipe Preparation Steps',
+        preparation_steps_is_html=False,is_published=True,)->Receita:
+        
+        if category_data is None:
+            category_data = {}
+        if author_data is None:
+            author_data = {}
+             
+        
+        return Receita.objects.create(
+            category=self.make_category(**category_data),
+            author=self.make_author(**author_data),
+            title=title,
+            description=description,
+            slug=slug,
+            preparation_time=preparation_time,
+            preparation_time_unit= preparation_time_unit,
+            servings=servings,
+            servings_unit=servings_unit,
+            preparation_steps=preparation_steps,
+            preparation_steps_is_html=preparation_steps_is_html,
+            is_published=is_published,
+        )
+    
--- receitas-drf/apps/receitas/tests/test_recipe_views.py
+++ receitas-drf/apps/receitas/tests/test_recipe_views.py
@@ -4,20 +4,18 @@ from django.urls import resolve, reverse
 from apps.receitas.views import home,categoria,receita
 from django.contrib.auth.models import User
 from .test_recipe_base import RecipeTestBase
-
+from unittest import skip
 
 class RecipeViewsTest(RecipeTestBase):
-    
-    def tearDown(self) -> None:
-        return super().tearDown()
-   
-    
+       
     def test_recipe_home_template_loads_recipes(self):
+        self.make_recipe(category_data={'name':'Bolo com cobertura de Leite Ninho'})
+        
         response = self.client.get(reverse('apps.receitas:home'))
         content = response.content.decode('utf-8')
         response_context_recipes = response.context['receitas']
 
-        self.assertIn('Recipe Title', content)
+        self.assertIn('Bolo com cobertura de Leite Ninho', content)
         self.assertIn('10 Minutos', content)
         self.assertIn('5 Porções', content)
         self.assertEqual(len(response_context_recipes), 1)
@@ -29,7 +27,7 @@ class RecipeViewsTest(RecipeTestBase):
     def test_recipe_home_view_returns_status_code_200_OK(self):
        response = self.client.get(reverse('apps.receitas:home'))
        self.assertEqual(response.status_code, 200)
-        
+               
     def test_recipe_home_view_loads_correct_template(self):
         response = self.client.get(reverse('apps.receitas:home'))        
         self.assertTemplateUsed(response, 'receitas/pages/home.html')
@@ -37,15 +35,7 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertTemplateUsed(response, 'receitas/partials/header.html')
         self.assertTemplateUsed(response, 'receitas/partials/search.html')
         self.assertTemplateUsed(response, 'receitas/partials/footer.html')
-    '''
-    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
-        response = self.client.get(reverse('apps.receitas:home'))
-        self.assertIn(
-            '<h1>No recipes found here 🥲</h1>',
-            response.content.decode('utf-8')
-        )
-    ''' 
-    
+      
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(
             reverse('apps.receitas:categoria', kwargs={'category_id': 1000})
@@ -65,6 +55,7 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertIs(view.func,receita)
     
     def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
+        self.make_recipe()
         response = self.client.get(
             reverse('apps.receitas:receita', kwargs={'id': 1000})
         )
@@ -72,7 +63,24 @@ class RecipeViewsTest(RecipeTestBase):
         response = self.client.get(reverse('apps.receitas:home'))
         content = response.content.decode('utf-8')
         response_context_recipes = response.context['receitas']
-        self.assertIn('Recipe Title', content)
+        self.assertIn('Bolo de Chocolate', content)
         self.assertIn('10 Minutos', content)
         self.assertIn('5 Porções', content)
         self.assertEqual(len(response_context_recipes), 1)
+    
+    #@skip("Apenas um teste de pulo de teste")        
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        
+        response = self.client.get(reverse('apps.receitas:home'))
+        self.assertIn(
+            '<h1>Não existe receitas 🥲</h1>',
+            response.content.decode('utf-8')
+        )
+    
+    def test_recipe_category_template_loads_recipes(self):
+        needed_title = 'This is a category test'
+        self.make_recipe(title=needed_title)
+        response = self.client.get(reverse('apps.receitas:categoria', args=(1,)))
+        content = response.content.decode('utf-8')
+        # Check if one recipe exists
+        self.assertIn(needed_title, content)
