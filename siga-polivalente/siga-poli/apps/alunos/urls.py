046749313ea660b88eb179f2099dd42bace32b62
andersonsimplicio <anderson25eu@gmail.com>
2025-05-14 11:42:25 -0300

    Siga

--- siga-polivalente/siga-poli/apps/alunos/urls.py
+++
@@ -1,6 +0,0 @@
-app_name = 'apps.alunos'
-
-
-urlpatterns = [
-
-]
--- siga-polivalente/siga-poli/apps/siga/form.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    username = forms.CharField(label="Matrícula", max_length=150)
-    password = forms.CharField(label="Senha", widget=forms.PasswordInput)
